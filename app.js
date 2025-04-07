562de82ca7ce6a28c78c3aaa466b49b71f486423
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-04-07 12:42:57 +0200

    ✨

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const useRouter = require("./route/user.route.js")
 const authRouter = require("./route/auth.route.js")
+const productRouter = require("./route/product.route.js")
 
 app.use(express.json());
 app.use('/user', useRouter);
 app.use('/auth', authRouter);
+app.use('/product',productRouter)
 
 module.exports = app;
