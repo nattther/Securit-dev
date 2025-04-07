29681ccfcc663741e798fbe33fcf923a474b7cbd
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-04-07 12:32:08 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app.js');
+
+const port = 3000;
+
+const server = http.createServer(app);
+
+server.on('listening',() => {
+    console.log("Server en route : " + 
+    port);
+})
+
+server.listen(port);
