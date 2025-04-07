f41dc5de0e3adfe3c8cfdd3046067130d4a8b4b2
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-04-07 12:31:58 +0200

    Create index.php

---
+++ 1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+    <div class="form-group">
+        <label for="">email</label>
+        <input type="email" id="email" name="email">
+    </div>
+    <div class="form-group">
+        <label for="">mot de passe</label>
+        <input type="password" id="password" name="password">
+    </div>
+    <button id="connect">Connexion</button>
+    </form>
+    <script src="index.js"></script>
+</body>
+</html>
